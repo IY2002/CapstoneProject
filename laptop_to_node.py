2f3474d594ee05503ff99ed31fe4e8be72dfa984
Daniel Colon <dec5476@psu.edu>
2024-04-16 01:09:08 -0400

    fixed json formatting again

--- laptop_to_node.py
+++ laptop_to_node.py
@@ -1,10 +1,10 @@
 import requests
 import socket
-
+import json
 def send_to_central():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     username = employee@shipitdone.com
-    info = {'username': username , 'ip' : ip}
+    info = json.dumps({'username': username , 'ip' : ip})
     requests.post('https://shipitdone.ngrok.app/laptop_signup', json = info)
    
