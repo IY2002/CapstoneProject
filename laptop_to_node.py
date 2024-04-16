6f5f0344b603473eefd58cdefa07c37e00f5a2a3
Daniel Colon <dec5476@psu.edu>
2024-04-16 01:06:10 -0400

    fixed json formatting

--- laptop_to_node.py
+++ laptop_to_node.py
@@ -5,6 +5,6 @@ def send_to_central():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     username = employee@shipitdone.com
-    info = {'username',ip}
+    info = {'username': username , 'ip' : ip}
     requests.post('https://shipitdone.ngrok.app/laptop_signup', json = info)
    
