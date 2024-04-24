792e8a9945f5a1b9161bcd1ebd15cada225d1f5d
Montage11225 <92657595+Montage11225@users.noreply.github.com>
2024-04-23 20:31:19 -0400

    Add files via upload

--- PrintingCode/laptop_networking.py
+++ PrintingCode/laptop_networking.py
@@ -24,11 +24,11 @@ def print_doc():
     printer = data['printer']
     doc_num = data['doc_num']
     if doc_num == 0:
-        urllib.request.urlretrieve('http://localhost:5005/invoice.pdf', 'D:/Capstone_Project/Additional file.pdf')
+        urllib.request.urlretrieve(url, 'D:/Capstone_Project/Additional file.pdf')
     elif doc_num == 1:
-        urllib.request.urlretrieve('http://localhost:5005/thank_you_note.pdf', 'D:/Capstone_Project/Additional file.pdf')
+        urllib.request.urlretrieve(url, 'D:/Capstone_Project/Additional file.pdf')
     else:
-        urllib.request.urlretrieve('http://localhost:5005/logo.pdf', 'D:/Capstone_Project/Additional file.pdf')
+        urllib.request.urlretrieve(url, 'D:/Capstone_Project/Additional file.pdf')
     if printer == 'Epson':
         webbrowser.open('https://qz.shipitdone.com/ARK_Additional.html')  # EPSON_print_doc()
     else:
